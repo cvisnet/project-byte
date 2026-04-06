acc06f72e8dceb8de7b814f08d33d14ab1611f67
drei-404 <andrei.ruiz.global@gmail.com>
2026-04-06 11:31:25 +0800

    chore: update package versions to fixed releases and modify dotenv import in prisma.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
   turbopack: {
     // absolute path; do not allow references outside this root
     root: path.resolve(__dirname),
-  },
+  }
 };
 
 export default nextConfig;
